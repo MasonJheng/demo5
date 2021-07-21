261acb6ddcb93391af2a904400aaf7c380a464ca
MasonJheng <masonjheng@gmail.com>
2021-07-21 20:32:22 +0800

    speak

--- demo5/ViewController.swift
+++ demo5/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loveTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func speak(_ sender: Any) {
+        
+        //print(loveTextField.text)
+        let speechUtterance = AVSpeechUtterance(string: loveTextField.text!)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+        
+    }
+    
 }
 
